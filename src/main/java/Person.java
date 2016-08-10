bd3fb35cca46bc106de6dba329c338699ff3e9c6
Er <free.ukrainev@gmail.com>
2016-08-10 21:54:49 +0300

    10-08-2016

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,61 +1,16 @@
-/*
 import java.io.PrintStream;
 
-*/
-/**
- * Created by ustenko on 10.08.16.
- *//*
+/* * Created by ustenko on 10.08.16.*/
+
 
 public class Person {
     String name;
 
-    public String Person(String personName) {
-        this.name = personName;
-        return this.name;
+    public Person(String personName) {
+        name = personName;
     }
 
     public String greet(String yourName) {
-        return String.format("Hi %s, my name is %s", this.name, yourName);
+        return String.format("Hi %s, my name is %s", yourName, name);
     }
 }
-public class Person {
-    String name;
-
-    public String Person(String personName) {
-        return this.name = personName;
-    }
-
-    public String greet(String yourName) {
-        return String.format("Hi s%, my name is s%", this.name, yourName);
-    }
-
-
-    public class Person {
-        String name;
-
-        public Person(String personName) {
-            name = personName;
-        }
-
-        public String greet(String yourName) {
-            return String.format("Hi %s, my name is %s", yourName, name);
-        }
-    }
-}
-
-public class Person {
-    String name;
-
-    public String Person(String personName) {
-        return name = personName;
-    }
-
-    public String greet(String yourName) {
-        return String.format("Hi s%, my name is s%", name, yourName);
-    }
-}
-*/
-
-
-
-
